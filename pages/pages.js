b69934646049936914c9e083ae0361a175118c19
chahat.nagar <chahat.nagar@digivalet.com>
2025-01-17 19:43:07 +0530

    changes for nextjs tutorial

---
+++ pages/pages.js
@@ -0,0 +1,5 @@
+export default function() {
+    return (
+        <h4>Chahat Nagar</h4>
+    )
+}
---
+++ pages/user/[username]/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+const Page = () => {
+    const router = useRouter();
+    console.log(router);
+
+    return (
+        <div>
+            <h1>This is index page for {router.query.username}</h1>
+            <button onClick={e => router}>Open Settings Page</button>
+        </div>
+    )
+}
+
+export default Page
